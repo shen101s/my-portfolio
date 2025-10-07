f3e52f60e5f41a12e83725ffca4f46415239ef56
Shen <eumagueshenshen@gmail.com>
2025-10-07 13:22:44 +0800

    Update icons

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,13 +4,6 @@ function Footer({ className }) {
     const name = 'Shenesis Eumague';
     const position = 'Full-stack Developer | Freelancer | Virtual Assistant';
 
-    const menus = [
-        { href:'https://facebook.com/', icon: FaFacebook },
-        { href:'https://instagram.com/', icon: FaInstagram },
-        { href:'https://linkedin.com/', icon: FaLinkedin },
-        { href:'mailto:eumagueshenshen@gmail.com', icon: FaMailBulk },
-    ];
-
     return (
         <>
             <section className="items-center mt-10 bg-gray-800" data-aos="fade-up">
@@ -19,24 +12,12 @@ function Footer({ className }) {
                         <div className="max-w-5xl mx-auto text-center">
                         
                             <h2 className="text-2xl font-bold mb-4">{name}</h2>
-                            <p className="text-gray-400 text-sm mb-6">
+                            <p className="text-gray-400 text-sm mb-4">
                                 {position}
                             </p>
 
-                            <div className="flex justify-center space-x-5 mb-6">
-                                {menus.map((menu, index) => {
-                                    const Icon = menu.icon;
-
-                                    return (
-                                        <a key={index} href={menu.href} target="_blank"className="hover:text-white transition">
-                                            <Icon size={20} />
-                                        </a>
-                                    )   
-                                })}
-                            </div>
-
                             <div className="text-xs text-gray-500">
-                            © {new Date().getFullYear()} {name}. All rights reserved.
+                            © {new Date().getFullYear()}. All rights reserved.
                             </div>
                         </div>
                     </footer>
